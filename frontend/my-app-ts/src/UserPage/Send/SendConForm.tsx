b63b36652b5ad15c1bb99dbb55dac9e42d3e134c
Kenta Watanabe <kenta2840006@g.ecc.u-tokyo.ac.jp>
2022-11-23 17:37:18 +0900

    1123

--- frontend/my-app-ts/src/UserPage/Send/SendConForm.tsx
+++ frontend/my-app-ts/src/UserPage/Send/SendConForm.tsx
@@ -11,29 +11,17 @@ import MenuItem from '@mui/material/MenuItem';
 import SendUserSelect from "./SendUserSelect";
 import { User, UserDemo } from "../../SignIn/User";
 import { UserContext } from '../../UserProvider';
-
+import Paper from '@mui/material/Paper';
+import { Con, toTimeCons } from "../Con";
 
 const theme = createTheme();
 
-export function SendConForm() {
+type SendConFormProps = {
+    setSentCons: React.Dispatch<React.SetStateAction<Con[]>>
+}
+
+export function SendConForm(props :SendConFormProps) {
     const {loginUser, setLoginUser} = React.useContext(UserContext);
-    const HandleSendCon = async (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        if (!data.get('conPoint')) {
-            alert("Please enter conPoint");
-            return;
-        } else if (!data.get('message')) {
-            alert("Please enter message");
-            return;
-        } else if (!sendUser) {
-            alert("Please select User");
-            return;
-        }
-        await axios.post('http://localhost:8000/send?user_id=' + loginUser.user_id, {sender: loginUser, receiver: sendUser, point: data.get('conPoint'), message: data.get('message')})
-        .then(() => {})
-        .catch((err) => {throw Error(`Failed to post con: ${err}`)});
-    };
     const [users, setUsers] = React.useState<User[]>([])
     const FetchOthers = async () => {
       await axios
@@ -44,50 +32,80 @@ export function SendConForm() {
     React.useEffect(() => {
         FetchOthers()
     },[]);
-    const [sendUser, setSendUser] = React.useState(UserDemo);
+    const [receiveUser, setReceiveUser] = React.useState(UserDemo);
+    const handleSendCon = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        if (!data.get('conPoint')) {
+            alert("Please enter conPoint");
+            return;
+        } else if (!data.get('message')) {
+            alert("Please enter message");
+            return;
+        } else if (!receiveUser) {
+            alert("Please select User");
+            return;
+        }
+        const pointornull = data.get('conPoint');
+        if (pointornull != null) {
+            const point :Number = +pointornull;
+            await axios.post("http://localhost:8000/send?user_id=" + loginUser.user_id, {sender: loginUser, receiver: receiveUser, point: point, message: data.get('message')})
+            .then((response :any) => {
+                const sent_cons: Con[] = toTimeCons(response.data.sent_cons);
+                if (sent_cons !== undefined) {
+                    props.setSentCons(sent_cons);
+                }
+            })
+            .catch((err) => {throw Error(`Failed to post con: ${err}`)});
+        }
+    };
 
     return (
         <ThemeProvider theme={theme}>
-            <Typography component="h1" variant="h6">
-                送る
-            </Typography>
-            <Box component="form" noValidate onSubmit={HandleSendCon} sx={{ mt: 3 }}>
-                <Grid container spacing={2}>
-                    <SendUserSelect 
-                        users={users}
-                        sendUser={sendUser}
-                        setSendUser={setSendUser}
-                    />
-                    <Grid item xs={12} sm={6}>
-                        <TextField
-                            id="conPoint"
-                            name="conPoint"
-                            label="送るConポイント"
-                            type="number"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            variant="standard"
-                        />
-                    </Grid>
-                    <Grid item xs={12} sm={100} width={500}>
-                    <TextField 
-                        fullWidth
-                        name="message"
-                        id="message" 
-                        label="メッセージ" 
-                        variant="standard" 
-                    />
-                    </Grid>
-                </Grid>
-                <Button
-                    type="submit"
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                >
-                    送信
-                </Button>
-            </Box>
+            <Grid item xs={12}>
+                <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+                    <Typography component="h1" variant="h6">
+                        送る
+                    </Typography>
+                    <Box component="form" noValidate onSubmit={handleSendCon} sx={{ mt: 3 }}>
+                        <Grid container spacing={2}>
+                            <SendUserSelect 
+                                users={users}
+                                receiveUser={receiveUser}
+                                setReceiveUser={setReceiveUser}
+                            />
+                            <Grid item xs={12} sm={6}>
+                                <TextField
+                                    id="conPoint"
+                                    name="conPoint"
+                                    label="送るConポイント"
+                                    type="number"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                    variant="standard"
+                                />
+                            </Grid>
+                            <Grid item xs={12} sm={100} width={500}>
+                            <TextField 
+                                fullWidth
+                                name="message"
+                                id="message" 
+                                label="メッセージ" 
+                                variant="standard" 
+                            />
+                            </Grid>
+                        </Grid>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            送信
+                        </Button>
+                    </Box>
+                </Paper>
+              </Grid>
         </ThemeProvider>
     )
 }
